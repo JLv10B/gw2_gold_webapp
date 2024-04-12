3b5d8d00dcf1ba6dcde843c897ffc313888fe4e2
James <jamesliaw10@gmail.com>
2024-04-11 22:38:05 -0700

    updated models

--- gw2_gold_webapp/unid_processing/models.py
+++ gw2_gold_webapp/unid_processing/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.    
-class GW2_Recipes(models.Model):
+class GW2_Crafting_Recipes(models.Model):
     recipe_id = models.IntegerField(unique = True)
     output_item_id = models.IntegerField(primary_key = True)
     output_item_count = models.IntegerField()
@@ -23,10 +23,7 @@ class GW2_Items(models.Model):
     item_id = models.IntegerField(primary_key = True, unique = True)
     item_name = models.CharField(max_length = 200)
     type = models.CharField(max_length = 200)
-    rarity = models.CharField(max_length = 200)
     venter_value = models.IntegerField(null = True, blank = True)
-    item_recipe = models.OneToOneField(GW2_Recipes, related_name = 'item', on_delete = models.SET_NULL, blank = True, null = True)
-    item_tp_data = models.OneToOneField(GW2_Trading_Post_Data, related_name = 'item', on_delete = models.SET_NULL, blank = True, null = True)
 
     def __str__(self) -> str:
         return self.item_name
@@ -40,58 +37,46 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return self.username
     
-class User_Default_Salvage_Rates(models.Model):
+class User_Salvage_Rates(models.Model):
     user = models.ForeignKey(CustomUser, on_delete = models.CASCADE)
-    item_id = models.IntegerField(primary_key = True, unique = True)
-    item_name = models.ForeignKey(GW2_Items, on_delete = models.CASCADE)
-    rarity = models.CharField() # TODO: Convert rarity to choice field
+    salvaged_item_id = models.IntegerField()
+    gained_item_id = models.IntegerField()
     salvage_rate = models.DecimalField(max_digits = 4, decimal_places = 2, null = True)
 
     def __str__(self) -> str:
-        return (f'item = {self.item_name}, salvage rate = {self.salvage_rate}')
+        return (f'item = {self.gained_item_id}, salvage rate = {self.salvage_rate}')
 
 
 class _User_Storage(models.Model):
     user = models.ForeignKey(CustomUser, on_delete = models.CASCADE)
-    time = models.DateTimeField(auto_now_add = True)
-    id = models.IntegerField(primary_key = True)
-    name = models.ForeignKey(GW2_Items, on_delete = models.CASCADE)
-    count = models.IntegerField(null = True)
+    time_recorded = models.DateTimeField(auto_now_add = True)
+    item_id = models.IntegerField()
+    item_count = models.IntegerField(null = True)
 
-class Raw_User_Shared_Inventory_Data(_User_Storage):
+class User_Shared_Inventory_Data(_User_Storage):
     pass
 
-class Raw_User_Inventory_Data(_User_Storage):
+class User_Inventory_Data(_User_Storage):
     pass
 
-class Raw_User_Bank_Data(_User_Storage):
+class User_Bank_Data(_User_Storage):
     pass
 
-
-# TODO: Why do I have aggregated storage tables instead of just using the raw data tables
-
-class Aggregated_User_Shared_Inventory(_User_Storage):
-    # Note: aggregate stacks of the same items when deserializing
+class User_Materials_Data(_User_Storage):
     pass
 
-class Aggregated_User_Inventory(_User_Storage):
-    # Note: aggregate stacks of the same items when deserializing
-    pass
-
-class Aggregated_User_Bank(_User_Storage):
-    # Note: aggregate stacks of the same items when deserializing
-    pass
-
-class User_Materials(_User_Storage):
-    pass
+class User_Wallet_Data(_User_Storage):
+    user = models.ForeignKey(CustomUser, on_delete = models.CASCADE)
+    time_recorded = models.DateTimeField(auto_now_add = True)
+    currency_id = models.IntegerField(primary_key = True)
+    item_count = models.IntegerField(null = True)
 
 class User_Salvage_Results(models.Model):
     username = models.ForeignKey(CustomUser, on_delete = models.CASCADE)
-    salvaged_item_id = models.IntegerField() # TODO: check if different rarities of unid gear have different ids
+    salvaged_item_id = models.IntegerField()
     salvaged_item_count = models.IntegerField()
-    item_id = models.IntegerField()
-    rarity = models.CharField() # TODO: Convert rarity to choice field
-    item_count = models.IntegerField()
+    gained_item_id = models.IntegerField()
+    gained_item_count = models.IntegerField()
 
 
 
