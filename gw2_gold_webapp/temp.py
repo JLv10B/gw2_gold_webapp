cf5503ffa90a2c781b98c76edf01c6a26060ae0f
James <jamesliaw10@gmail.com>
2024-06-24 12:28:28 -0700

    first test test

---
+++ gw2_gold_webapp/temp.py
@@ -0,0 +1,4 @@
+f = open('test_file.txt', 'w+')
+f.write('test')
+f.write('one')
+f.close()
--- gw2_gold_webapp/unid_processing/views.py
+++ gw2_gold_webapp/unid_processing/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from .models import GW2_Recipes
-from .serializer import GW2_Recipes_Serializer
 import requests
+from django.http import HttpResponse
+import json
 # Create your views here.
 
 # hyperbrand
@@ -18,30 +18,54 @@ single_recipe = requests.get(f'https://api.guildwars2.com/v2/recipes/{recipe_id}
 # print(character_inventory_items.text)
 # print(account_bank_items.text)
 # print(all_recipes.text)
-print(single_recipe.json())
-print(type(single_recipe.json()))
+# print(single_recipe.json())
+# single_recipe_dict = json.dumps(single_recipe.json())
+# for key in single_recipe_dict:
+#     print(key)
+# file = open('test.txt', 'w+')
+# file.writelines(single_recipe_string)
+# file.close()
 # for char in account_bank_items:
     # print(char)
 
-def GW2_recipes_PUT_GET(request, recipe_id = None):
-    """
-    This function accepts put and get requests. 
+# def GW2_recipes_PUT_GET(request, recipe_id = None):
+#     """
+#     This function accepts put and get requests. 
     
-    Put = updates all recipes
-    Get = returns recipes in recipe_id_list
-    """
-    if request.method == "GET":
-        all_recipe_json = requests.get(f'https://api.guildwars2.com/v2/recipes')
-        recipe_id_list = all_recipe_json.json()
-        for recipe_id in recipe_id_list:
-            recipe_request = requests.get(f'https://api.guildwars2.com/v2/recipes/{recipe_id}')
-            recipe = recipe_request.json()
-            serialized_recipe = GW2_Recipes_Serializer(data = recipe)
-            serialized_recipe.is_valid()
-            serialized_recipe.save()
+#     Put = updates all recipes
+#     Get = returns recipes in recipe_id_list
+#     """
+#     if request.method == "GET":
+#         all_recipe_json = requests.get(f'https://api.guildwars2.com/v2/recipes')
+#         recipe_id_list = all_recipe_json.json()
+#         for recipe_id in recipe_id_list:
+#             recipe_request = requests.get(f'https://api.guildwars2.com/v2/recipes/{recipe_id}')
+#             recipe = recipe_request.json()
+#             serialized_recipe = GW2_Recipes_Serializer(data = recipe)
+#             serialized_recipe.is_valid()
+#             serialized_recipe.save()
 
-    if request.method == "GET":
+#     if request.method == "GET":
+#         pass
+
+def data_dump_view(request):
+    """
+    This function is testing out dumping data into local text file
+
+    1.) pull data from API
+    2.) Create a file with the data
+    3.) Save the file to local
+    """
+    response = HttpResponse(headers={"Content-Type": "text/plain",
+                                     "Content-Disposition": 'attachment; filename="test.txt"'},)
+    
         
+    lines = ["This is line 1\n",
+            "This is line 2\n",
+            "this is line 3\n"]
+    
+    response.writelines(lines)
+    return response
         
     
     
