601de86e1878c84c379a74bbd95292f5e224146f
James <jamesliaw10@gmail.com>
2024-07-08 18:18:59 -0700

    pull_user_raw_data_view update

--- gw2_gold_webapp/unid_processing/views.py
+++ gw2_gold_webapp/unid_processing/views.py
@@ -11,12 +11,13 @@ from rest_framework.throttling import AnonRateThrottle, UserRateThrottle
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework.decorators import api_view
-from .models import CustomUser
+from .models import CustomUser, User_Salvage_Records
 from .serializer import CustomUser_Serializer
 from django.views.decorators.cache import cache_page
 from django.views.decorators.vary import vary_on_headers
 import requests
 import json
+import os.path
 # Create your views here.
 
 class CustomUser_List_ViewSet(generics.ListAPIView):
@@ -30,38 +31,55 @@ class CustomUser_List_ViewSet(generics.ListAPIView):
 @api_view(['GET'])
 def Pull_User_Raw_Data_View(request):
     """
-    This view performs API calls to GW2 Bank, Character inventories, Shared inventory, Materials, Wallet APIs to retrieve current items, returns dictionary {item_id:quantity}. Store data in cache as initial or final
+    This view performs API calls to GW2 Bank, Character inventories, Shared inventory, Materials, Wallet APIs to retrieve current items, returns dictionary {item_id:quantity}. Saves data in local files for developement purposes only. Future data storage will be in S3 or other noSQL database.
 
     char_inventory:
-    {'bags': 
-        [
-            {'inventory': 
-                [
+    [
+      {'bags': 
+           [
+              {'inventory': 
+                 [
                     {'id':1234,
                      'count: 1,
                     },
-                ]  
-            },
-            {'inventory': 
-                [
+                 ]  
+               },
+              {'inventory': 
+                 [
                     {'id':4321,
                      'count: 2,
                     },
-                ]  
-            },
-             
-        ]
-    }
+                 ]  
+               },
+                
+            ]
+       }
+    ]
+
+    bank_items/shared inventory/materials/wallet:
+    [
+      {
+        "id": 123,
+        "count": 1,
+        ...
+      },
+      {
+        "id": 122,
+        "count": 1,
+        ...
+      },
+      ...
+    ]
     """
     if request.method == "GET":
-        key = request.user.api_key
+        user_api = request.user.api_key
         account_item_dict = {}
 
         # TODO: Can we optimize pulling data from GW2 API to decrease runtime?
 
-        char_list = requests.get(f'https://api.guildwars2.com/v2/characters/?access_token={key}').json()
+        char_list = requests.get(f'https://api.guildwars2.com/v2/characters/?access_token={user_api}').json()
         for char in char_list:
-            char_inventory = requests.get(f'https://api.guildwars2.com/v2/characters/{char}/inventory?access_token={key}').json()
+            char_inventory = requests.get(f'https://api.guildwars2.com/v2/characters/{char}/inventory?access_token={user_api}').json() 
             bag_list = char_inventory['bags']
             for bag_slot in bag_list:
                 if bag_slot != None:
@@ -75,8 +93,65 @@ def Pull_User_Raw_Data_View(request):
                         else:
                             account_item_dict[item_id] = item_count
 
-        account_items_json = json.dumps(account_item_dict)        
-        return HttpResponse(account_items_json)
+        bank_items =  requests.get(f'https://api.guildwars2.com/v2/account/inventory?access_token={user_api}').json() 
+        for item_slot in bank_items:
+            if item_slot != None:
+                item_id = item_slot['id']
+                item_count = item_slot['count']
+            if item_id in account_item_dict:
+                account_item_dict[item_id] += item_count
+            else:
+                account_item_dict[item_id] = item_count
+
+        shared_inventory_items = requests.get(f'https://api.guildwars2.com/v2/account/bank?access_token={user_api}').json() 
+        for item_slot in shared_inventory_items:
+            if item_slot != None:
+                item_id = item_slot['id']
+                item_count = item_slot['count']
+            if item_id in account_item_dict:
+                account_item_dict[item_id] += item_count
+            else:
+                account_item_dict[item_id] = item_count
+
+        materials = requests.get(f'https://api.guildwars2.com/v2/account/materials?access_token={user_api}').json()
+        for item_slot in materials:
+            if item_slot != None:
+                item_id = item_slot['id']
+                item_count = item_slot['count']
+            # TODO: Find a better way to handle materials that have value = 0
+            if item_count != 0:
+                if item_id in account_item_dict:
+                    account_item_dict[item_id] += item_count
+                else:
+                    account_item_dict[item_id] = item_count 
+
+        wallet = requests.get(f'https://api.guildwars2.com/v2/account/wallet?access_token={user_api}').json()
+        coins = wallet[0]
+        coins_value = coins['value']
+        if 'coin' in account_item_dict:
+            account_item_dict['coin'] += coins_value
+        else:
+            account_item_dict['coin'] = coins_value  
+
+        try:
+            new_record_number = User_Salvage_Records.objects.get(user = request.user).record_number + 1
+        except:
+            new_record_number = 1
+
+        # TODO: Need to check if this actually checks if the correct file exists
+        if os.path.isfile(f'{request.user}/{new_record_number}/initial_record.json'):
+            file_name = 'final_record'
+        else:
+            file_name = 'initial_record'
+
+        # TODO: Need to change where the path saves to and creates new dir as needed
+        response = HttpResponse(content_type='application/json')  
+        response['Content-Disposition'] = f'attachment; filename="{request.user}/{new_record_number}/{file_name}.json"'
+
+        response.write(json.dumps(account_item_dict))
+
+        return response
+
 
 def Create_User_Salvage_Record_and_Outcome_Data_View(request, initial_record, final_record = None):
     """
